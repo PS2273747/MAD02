73304202daa5fff6044a541388b44ec37ab3fbe8
ps227374 <ps227374@summacollege.nl>
2023-06-02 16:34:30 +0200

    changed user profile layout, added showdialoug

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -72,55 +72,96 @@ class _ProfileScreenState extends State<ProfileScreen> {
         actions: [
           IconButton(
             icon: Icon(Icons.info_outline),
-            onPressed: () {},
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AboutDialog(
+                    applicationName: 'Music APP',
+                    applicationVersion: '0.1.4',
+                    applicationLegalese: 'Kinga © 2023',
+                  );
+                },
+              );
+            },
           ),
         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Name:',
-              style: TextStyle(fontSize: 20.0),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            color: Colors.white,
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Name:',
+                  style: TextStyle(fontSize: 20.0, color: Colors.blue),
+                ),
+                SizedBox(height: 10.0),
+                Text(
+                  _name,
+                  style: TextStyle(fontSize: 24.0, color: Colors.blue),
+                ),
+                SizedBox(height: 20.0),
+                Text(
+                  'Email:',
+                  style: TextStyle(fontSize: 20.0, color: Colors.blue),
+                ),
+                SizedBox(height: 10.0),
+                Text(
+                  _email,
+                  style: TextStyle(fontSize: 24.0, color: Colors.blue),
+                ),
+              ],
             ),
-            SizedBox(height: 10.0),
-            Text(
-              _name,
-              style: TextStyle(fontSize: 24.0),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.blue,
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Friends:',
+                    style: TextStyle(fontSize: 20.0, color: Colors.white),
+                  ),
+                  SizedBox(height: 10.0),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: _friends.length,
+                      itemBuilder: (context, index) {
+                        return Text(
+                          _friends[index],
+                          style: TextStyle(fontSize: 18.0, color: Colors.white),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
-            SizedBox(height: 20.0),
-            Text(
-              'Email:',
-              style: TextStyle(fontSize: 20.0),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              _email,
-              style: TextStyle(fontSize: 24.0),
-            ),
-            SizedBox(height: 30.0),
-            Text(
-              'Friends:',
-              style: TextStyle(fontSize: 20.0),
-            ),
-            SizedBox(height: 10.0),
-            Column(
-              children: _friends.map((friend) => Text(friend)).toList(),
-            ),
-            SizedBox(height: 30.0),
-            ElevatedButton(
-              onPressed: _performLogout,
-              child: Text('Logout'),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
       bottomNavigationBar: BottomNav(
         currentIndex: 0,
         onItemTapped: (index) {
           // No navigation logic here, handled in BottomNav widget
         },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _performLogout,
+        label: Text(
+          'Log Out',
+          style: TextStyle(
+            color: Colors.blue,
+          ),
+        ),
+        backgroundColor: Colors.white,
       ),
     );
   }
